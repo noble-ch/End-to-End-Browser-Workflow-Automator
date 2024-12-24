ef8931bde1a354223b52edbcadd52a73abbfd747
Eyerusalem-Hailemariam <eyerusalem008@gmail.com>
2024-12-25 00:08:37 +0300

    updated

--- src/pages/api/fetchData.js
+++ src/pages/api/fetchData.js
@@ -1,23 +1,17 @@
-<<<<<<< HEAD
-//pages/api/fetchData.js
-// pages/api/fetchData.js
-=======
->>>>>>> e32bf1d49b3692b98b29be6ce942b82e72656f7f
 import { connectToDatabase } from '../../lib/mongodb';
 
 export default async function handler(req, res) {
   try {
     const db = await connectToDatabase();
-<<<<<<< HEAD
-   
-    const data = await db.collection('records').find().toArray();
-    res.status(200).json(data); 
-=======
-    // Perform operations on the database
-    const data = await db.collection('WEBAUTOMATION_COLLECTION').find().toArray(); 
-    res.status(200).json(data);
->>>>>>> e32bf1d49b3692b98b29be6ce942b82e72656f7f
+
+    // Fetch data from both collections
+    const recordsData = await db.collection('records').find().toArray();
+    const webAutomationData = await db.collection('WEBAUTOMATION_COLLECTION').find().toArray();
+
+    // Combine the data or send them separately
+    res.status(200).json({ records: recordsData, webAutomation: webAutomationData }); 
   } catch (error) {
     res.status(500).json({ error: 'Failed to fetch data' });
   }
 }
+
